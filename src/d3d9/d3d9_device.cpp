c674453eae742d95afd2def4f235edb45cf5170b
Holger Frydrych <holger@frydrych.org>
2026-06-15 20:26:07 +0200

    Moving OpenVR commands back to the queue thread for stability

--- src/d3d9/d3d9_device.cpp
+++ src/d3d9/d3d9_device.cpp
@@ -37,8 +37,6 @@
 #pragma fenv_access (on)
 #endif
 
-VkSubmitThreadCallback *g_pVkSubmitThreadCallback = nullptr;
-
 namespace dxvk {
 
   D3D9DeviceEx::D3D9DeviceEx(
@@ -4288,6 +4286,7 @@ namespace dxvk {
     }
 
     //OpenVRDirectMode::Get()->PrePresent(this);
+  	g_hl2vr->OnPrePresent(this);
 
     HRESULT result = m_implicitSwapchain->Present(
       pSourceRect,
